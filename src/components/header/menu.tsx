73e47219a12c49edbd097c566096d9f93b93d51e
studioj2y <61308545+studioj2y@users.noreply.github.com>
2025-08-24 01:31:38 +0800

    Update menu.tsx

--- src/components/header/menu.tsx
+++ src/components/header/menu.tsx
@@ -1,16 +1,16 @@
 import { motion } from "framer-motion"
 
-// function ThemeToggle() {
-//   const { isDark, toggleDark } = useDark()
-//   return (
-//     <li onClick={toggleDark} className="cursor-pointer [&_*]:cursor-pointer transition-all">
-//       <span className={$("inline-block", isDark ? "i-ph-moon-stars-duotone" : "i-ph-sun-dim-duotone")} />
-//       <span>
-//         {isDark ? "浅色模式" : "深色模式"}
-//       </span>
-//     </li>
-//   )
-// }
+ function ThemeToggle() {
+   const { isDark, toggleDark } = useDark()
+   return (
+     <li onClick={toggleDark} className="cursor-pointer [&_*]:cursor-pointer transition-all">
+       <span className={$("inline-block", isDark ? "i-ph-moon-stars-duotone" : "i-ph-sun-dim-duotone")} />
+       <span>
+         {isDark ? "浅色模式" : "深色模式"}
+       </span>
+     </li>
+   )
+ }
 
 export function Menu() {
   const { loggedIn, login, logout, userInfo, enableLogin } = useLogin()
